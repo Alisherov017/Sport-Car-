85c2bc2089aa0e015d2017f0c166f726c497775b
Alisherov017 <r.alisherov2005@gmail.com>
2024-11-14 14:59:06 +0600

    content

---
+++ components/CarCard.tsx
@@ -0,0 +1,26 @@
+import { CarProps } from "@/types";
+import React from "react";
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  return (
+    <div className="car-card group ">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title ">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p className="">
+        <span className="">Car Rent...</span>
+      </p>
+    </div>
+  );
+};
+
+export default CarCard;
