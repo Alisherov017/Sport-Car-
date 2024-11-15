1f6af81a678b43374645a8d89b62d0b5543859da
Alisherov017 <r.alisherov2005@gmail.com>
2024-11-15 16:09:53 +0600

    cards

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,5 +1,8 @@
 import { CarProps } from "@/types";
+import { calculateCarRent } from "@/utils";
+import Image from "next/image";
 import React from "react";
+import CustomButton from "./CustomButton";
 
 interface CarCardProps {
   car: CarProps;
@@ -8,7 +11,10 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
+    // card
     <div className="car-card group ">
       <div className="car-card__content">
         <h2 className="car-card__content-title ">
@@ -16,9 +22,55 @@ const CarCard = ({ car }: CarCardProps) => {
         </h2>
       </div>
 
-      <p className="">
-        <span className="">Car Rent...</span>
+      <p className="flex mt-6 text-[32px] font-extrabold ">
+        <span className="self-start text-[14px] font-semibold ">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium ">/day</span>
       </p>
+
+      {/* image */}
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          alt="car modal"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+
+      {/* additional information */}
+      <div className="relative flex w-full mt-2 ">
+        <div className="flex group-hover:invisible w-full justify-between text-gray ">
+          <div className="flex flex-col justify-center items-center gap-2 ">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            <p className="text-[14px]  ">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2 ">
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className="text-[14px]  ">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2 ">
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+            <p className="text-[14px]  "> {city_mpg} MPG </p>
+          </div>
+        </div>
+
+        {/* button */}
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue "
+          />
+        </div>
+      </div>
     </div>
   );
 };
