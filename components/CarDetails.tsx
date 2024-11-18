26bde6b0fd3a41c806e1c22984d8ea52fe02a25e
Alisherov017 <r.alisherov2005@gmail.com>
2024-11-18 17:33:51 +0600

    card

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { CarProps } from "@/types";
 import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
 import React, { Fragment } from "react";
 
 interface CarDetailsProps {
@@ -12,11 +15,62 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          className="realtive z-10  "
-          onClose={closeModal}
-        ></Dialog>
+        <Dialog as="div" className="realtive z-10  " onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25 " />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100  scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100  scale-100"
+                leaveTo="opacity-0  scale-95"
+              >
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5 ">
+                  <button
+                    className="absolute top-2 right-2 z-10 w-fit bg-primary-blue-100 rounded-full "
+                    type="button"
+                    onClick={closeModal}
+                  >
+                    <Image
+                      src="/close.svg"
+                      alt="close"
+                      width={20}
+                      height={20}
+                      className="object-contain"
+                    />
+                  </button>
+
+                  {/* content of modalcard */}
+                  <div className="flex-1 flex flex-col gap-3 ">
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg ">
+                      <Image
+                        src="/hero.png"
+                        alt="car modal"
+                        fill
+                        priority
+                        className="object-contain"
+                      />
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
       </Transition>
     </>
   );
